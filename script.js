a20048af75f575933658f3f6a48528ce7c3b17c1
fedepkz <federicopupkiewicz@gmail.com>
2021-04-05 21:45:48 -0300

    TP3 JS CHECKBOX

--- script.js
+++ script.js
@@ -11,5 +11,35 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function addTodo() {
-  alert('Boton Add TODO clickeado!')
+  
+  var tarea = prompt('Ingrese la tarea', 'Completar');
+  
+  while (tarea == null || tarea == ''){
+    var tarea = prompt('Error! Ingrese la tarea', 'Completar')
+  };
+  li = document.createElement('li');
+  checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.value = 1;
+  checkbox.name = "test";
+  checkbox.onclick = contador;
+  li.appendChild(checkbox);
+  li.appendChild(document.createTextNode(tarea));  
+  list.appendChild(li);
+  contador();
+  
+};
+function contador(){
+  
+  var checkLength = 0;
+  var boxes = list.getElementsByTagName("input");
+  for (var i = 0; i < boxes.length; i++)
+  {
+      boxes[i].checked ? checkLength++ : null;
+      
+  };
+  itemCountSpan.innerHTML = checkLength;
+  
+  uncheckedCountSpan.innerHTML = boxes.length-checkLength;
+  
 }
